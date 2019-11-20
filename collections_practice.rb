cfd5116ee913dbc85eb0d06d0826d36302e60925
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-20 05:59:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,12 +1,3 @@
 def sort_array_asc(numbers)
-  sorted_nums = numbers.sort do |a, b|
-    if (a > b)
-      -1
-    elsif (a == b)
-      0
-    elsif (a < b)
-      1
-    end
-  end
-  sorted_nums
+  numbers.sort
 end
