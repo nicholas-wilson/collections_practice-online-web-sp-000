4d02606d265ec41cb76d8b486a3d8eff42dc3ca1
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-20 06:03:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,16 @@
 def sort_array_asc(numbers)
   numbers.sort
 end
+
+def sort_array_desc(numbers)
+  numbers.sort do |a, b|
+    if a == b
+      0
+    elsif a > b
+      -1
+    elsif a < b
+      1
+    end
+  end
+  numbers
+end
